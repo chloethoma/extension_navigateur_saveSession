8a6bb988e75363cd272d3c15a6e3ef8a6a2b6399
Chloé T. <chloe.thomas14@gmail.com>
2024-01-26 16:39:14 +0100

    MVP done

--- sidepanel.js
+++ sidepanel.js
@@ -1,34 +1,56 @@
-const displayWhenOpenSidePanel = async () => {
+// Itérations :
+// Clean le code
+
+// Print la liste des sessions à l'ouverture du SidePanel
+const printSessionList = async () => {
+  // Récupère les infos qui on été stockées dans le storage
   const sessionsFromStorage = await chrome.storage.local.get()
-  // console.log("sessionsFromStorage :", sessionsFromStorage)
+
+  // Reset et print toutes les infos dans le SidePanel (titre de la session et nombre de tabs)
+  document.getElementById("sessionList").innerHTML=""
   for (const session in sessionsFromStorage) {
-    displaySession(sessionsFromStorage[session], session)
+    printSession(sessionsFromStorage[session], session)
   }
 }
-displayWhenOpenSidePanel()
+printSessionList()
 
+// Print une session
+const printSession = (sessionArray, sessionTitle) => {
+  // Print le template prédéfini dans index.html et insère le titre de la session et le nombre de tabs correspondant
+  const template = document.getElementById("template")
+  const element = template.content.firstElementChild.cloneNode(true)
+  element.querySelector(".title").textContent = sessionTitle
+  element.querySelector(".tabsNumber").textContent = `${sessionArray.length} tabs`
+  document.getElementById("sessionList").append(element)
+}
+
+// Delete tout le storage et le SidePanel --> refaire deux fonctions séparées
 const deleteStorageAndClearSidePanel = async () => {
+  // Clear tout le storage
   await chrome.storage.local.clear()
-  const parentDiv = document.querySelector(".tabsSessions")
+
+  // Supprime tous les éléments enfants de la div Tabs Session
+  const parentDiv = document.getElementById("sessionList")
   let childDiv = parentDiv.firstChild
-  
   while (childDiv) {
     parentDiv.removeChild(childDiv)
     childDiv = parentDiv.firstChild
   }
 }
 
-const displaySession = (sessionArray, sessionTitle) => {
-  const template = document.getElementById("template")
-  const elements = new Set()
-  const element = template.content.firstElementChild.cloneNode(true)
-  element.querySelector(".title").textContent = sessionTitle
-  element.querySelector(".tabsNumber").textContent = `${sessionArray.length} tabs`
-  elements.add(element)
-  document.querySelector(".tabsSessions").append(...elements)
+// Supprime une session du storage + reprint la sessionList pour mise à jour sur le SidePanel
+const deleteOneSession = async (event) => {
+  // Récupère le titre de la session correspondant à l'event click
+  const sessionTitle = event.target.parentNode.children[0].textContent
+  // Delete la session correspondante stockée dans le storage
+  await chrome.storage.local.remove([sessionTitle])
+  // Reprint la sessionList dans le SidePanel
+  printSessionList()
 }
 
-const saveSession = async () => {
+// Récupère les datas de la current Window, enregistre les données dans le storage, demande un titre de session, print la session sur le sidePanel
+const addSessionToTheList = async () => {
+  // Récupère les data des tabs ouverts dans la current Window
   const getTabsData = async () => {
     // let currentWindow = await chrome.windows.getCurrent({})
     // let allTabsData = await chrome.tabs.query({windowId:currentWindow.id})
@@ -37,25 +59,39 @@ const saveSession = async () => {
   }
 
   const sessionTitle = prompt("Quel est le nom de ta session ?")
-  
-  const sessionTabsData = await getTabsData()
-  console.log("allTabsData : ", sessionTabsData)
-  await chrome.storage.local.set({[sessionTitle]:sessionTabsData})
+  const sessionData = await getTabsData()
 
-  displaySession(sessionTabsData, sessionTitle)
+  // Enregistre les données dans le storage avec la clé = titre de la session
+  await chrome.storage.local.set({ [sessionTitle]: sessionData })
+  // Print la session sur le sidePanel
+  printSession(sessionData, sessionTitle)
 }
 
-const restoreSessionInNewWindow = () => {
-
+// Ouvre une nouvelle window avec toutes les tabs enregistrées
+const openSessionInNewWindow = async (event) => {
+  const sessionTitle = event.target.parentNode.children[0].textContent
+  const dataFromStorage = await chrome.storage.local.get()
+  const urlArray = []
+  for (const element of dataFromStorage[sessionTitle]) {
+    urlArray.push(element.url)
+  }
+  chrome.windows.create({ url: urlArray })
 }
 
 document.getElementById("saveButton")
-  .addEventListener("click", saveSession)
+  .addEventListener("click", addSessionToTheList)
 
-document.getElementById("deleteButton")
+document.getElementById("deleteAllSession")
   .addEventListener("click", deleteStorageAndClearSidePanel)
 
-
+document.getElementById("sessionList")
+  .addEventListener("click", (event) => {
+    if (event.target.className === "deleteSession") {
+      deleteOneSession(event)
+    } else if (event.target.parentNode.className === "session") {
+      openSessionInNewWindow(event)
+    }
+  })
 
 
 
